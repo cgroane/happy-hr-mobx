ba1607c533e0b6d6b5aa9a292c305d585ef61d45
Connor Roane <connorroane@Connors-MacBook-Pro.local>
2018-04-16 15:04:50 -0500

    add autocomplete when typing in restaurant name

--- src/components/AddNew/AddNew.js
+++ src/components/AddNew/AddNew.js
@@ -4,13 +4,14 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link, withRouter} from 'react-router-dom';
 import axios from 'axios';
+import PlacesAutocomplete, {geocodeByAddress, getLatLng} from 'react-places-autocomplete';
 import {sortDeals, getLocations, setDistance} from './../../ducks/reducer';
 import appStyle from './../styles/appStyle';
 import AddNewStyle from './AddNewStyle';
 import functions from './../utility/functions';
 import './AddNew.css';
 
-// const google = window.google;
+const google = window.google;
 
 class AddNew extends Component {
     constructor(props) {
@@ -20,7 +21,7 @@ class AddNew extends Component {
             details: '',
             newDays: [],
             name: '',
-            addressOne: '',
+            address: '',
             city: '',
             state: '',
             zip: ''
@@ -29,7 +30,9 @@ class AddNew extends Component {
         this.handleDaySelect = this.handleDaySelect.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
+    componentDidMount() {
+        
+    }
     handleChange(event) {
         const value = event.target.value;
         const name = event.target.name;
@@ -50,6 +53,41 @@ class AddNew extends Component {
                     newDays: filterDay
             })
         }
+    }
+    handleSelect = (address) => {
+        geocodeByAddress(address)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => console.log('Success', latLng))
+            .catch(error => console.error('Error', error))
+    }
+    handleRestaurant = () => {
+        // console.log(google.maps.places)
+        var autocomplete = new google.maps.places.Autocomplete(this.restaurant)
+        var that = this
+        autocomplete.addListener('place_changed', function() {
+            var place = autocomplete.getPlace()
+            if (!place.geometry) {
+                alert('not good')
+            }
+            // place.split(',')
+            var address = place.formatted_address.split(',')
+            console.log(address)
+            console.log(address[2].split(' '));
+            console.log(address[2][0])
+            address[2] = address[2].split(' ')
+            that.assignValue(that.city, address[1])
+            that.assignValue(that.state1, address[2][1])
+            that.assignValue(that.address, address[0])
+            that.assignValue(that.restaurant, place.name)
+            that.assignValue(that.zip, address[2][2])
+        })
+        console.log(this.city)
+        
+    }
+    assignValue = (target, text) => {
+        
+        target.value = text
+        this.setState({[target.name]: text})
     }
     handleSubmit(event) {
         function getRandomInt(min, max) {
@@ -95,16 +133,16 @@ class AddNew extends Component {
         const daysOptions = functions.daysBoxes(["Monday", "Tuesday", "Wenesday", "Thursday", "Friday", "Saturday", "Sunday"], this.handleDaySelect)
         
         return (
-            <div>
+           <div>
             <form id="newDeal" className={`${AddNewStyle.formContainer}`} >
-                <input type="text" className={`${AddNewStyle.formText}`} ref={(ref) => this.title = ref} name="title" onChange={this.handleChange} placeholder="Headline..." />Title
-                <input type="text" className={`${AddNewStyle.formText}`} ref={(ref) => this.details = ref} name="details" onChange={this.handleChange} placeholder="ex. 4-8pm..." />Details            
-                <input type="text" className={`${AddNewStyle.formText}`} ref={(ref) => this.restaurant = ref} name="name" onChange={this.handleChange} placeholder="Name of bar or restaurant..." />Restaurant
+                            
+                <input type="text" className={`${AddNewStyle.formText}`} ref={(ref) => this.restaurant = ref} name="name" onChange={this.handleRestaurant} placeholder="Name of bar or restaurant..." />Restaurant
                 <input type="text" className={`${AddNewStyle.formText}`} ref={(ref) => this.address = ref} name="addressOne" onChange={this.handleChange} placeholder="Street address..." />Address
                 <input type="text" className={`${AddNewStyle.formText}`} ref={(ref) => this.city = ref} name="city" onChange={this.handleChange} placeholder="City" />City
                 <input type="text" className={`${AddNewStyle.formText}`} ref={(ref) => this.state1 = ref} name="state" onChange={this.handleChange} placeholder="ex. TX" />State
                 <input type="text" className={`${AddNewStyle.formText}`} ref={(ref) => this.zip = ref} name="zip" onChange={this.handleChange} placeholder="5 numbers" />Zip Code
-                
+                <input type="text" className={`${AddNewStyle.formText}`} ref={(ref) => this.title = ref} name="title" onChange={this.handleChange} placeholder="Headline..." />Title
+                <input type="text" className={`${AddNewStyle.formText}`} ref={(ref) => this.details = ref} name="details" onChange={this.handleChange} placeholder="ex. 4-8pm..." />Details
                 <div className={`${AddNewStyle.checkBoxContainer}`} >
                     {daysOptions}
                 </div>
